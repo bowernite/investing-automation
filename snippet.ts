aade87afc1a43c6dfddd5adbb2d3947db7ca46cd
Brett <bjabramczyk@gmail.com>
2024-09-06 15:18:41 -0500

    fix prompt

--- snippet.ts
+++ snippet.ts
@@ -161,9 +161,10 @@
   }
 
   function getAmountToSell(): number {
-    const isSelling = confirm(
-      "Would you like to withdraw money from your taxable account?"
+    const userInput = prompt(
+      "Would you like to withdraw money from your taxable account? (Type 'y' for yes)"
     );
+    const isSelling = userInput?.toLowerCase() === "y";
     if (!isSelling) return 0;
 
     const sellInput = prompt("How much would you like to sell?");
